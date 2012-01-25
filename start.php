0105d46871891405b40c3aaa1b413a188a87e29a
Atze Peng <info@indygfx.de>
2012-01-25 09:44:41 +0100

    new idea... going by my self

--- start.php
+++ start.php
@@ -1,15 +1,8 @@
 <?php
-elgg_register_event_handler('init', 'system', 'cgp_theme_init');
-
-function cgp_theme_init() {
-	        elgg_extend_view('css/elgg', 'cgp_theme/css');
-		elgg_unregister_menu_item('topbar', 'elgg_logo');
-                elgg_extend_view('css/admin', 'cgp_theme/admin'); 
-
-
-		$base = elgg_get_plugins_path() . 'cgp_theme/actions/cgp_theme';
-		elgg_register_action('cgp_theme/settings/save', "$base/save.php", 'admin');
-
-
-}
+ 
+    function cgp_theme_init() {
+        //elgg_extend_view('css/elgg', 'cgp_theme/css');
+    }
+ 
+    elgg_register_event_handler('init', 'system', 'cgp_theme_init');
 ?>
